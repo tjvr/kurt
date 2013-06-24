d41de37697c25ff90b11c9145cad58f567555088
blob8108 <blob8108@gmail.com>
2013-06-24 20:38:58 +0100

    Doc for kurt.text

--- kurt/text/__init__.py
+++ kurt/text/__init__.py
@@ -15,11 +15,28 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with Kurt. If not, see <http://www.gnu.org/licenses/>.
 
+"""Experimental block plugin syntax parser.
+
+The syntax is the same as the ``scratchblocks`` tag used on the Scratch Forums
+and Wiki. See: http://wiki.scratch.mit.edu/wiki/Block_Plugin
+
+Soon to be replaced by a new parser with a much less restrictive syntax (but
+keeping the same interface).
+
+"""
+
 import ply
 
 from lexer import lex, tokens
 from parser import block_plugin_parser
 
-def parse_block_plugin(text, project):
-    script = block_plugin_parser.parse(text, tracking=True)
-    return script
+def parse(text, scriptable):
+    """Return a :class:`kurt.Script` from the given code syntax.
+
+    :param text: Code with blocks separated by ``\\n``.
+
+    :param scriptable: The :class:`.kurtScriptable` object the script will be
+                       added to, for context.
+
+    """
+    return block_plugin_parser.parse(text, tracking=True)
